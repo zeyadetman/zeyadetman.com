b0aa002d1fa9f100c59eeda3dc470e5b6130f23e
zeyadetman <zeyadetman@gmail.com>
2021-09-14 05:07:53 +0200

    update

---
+++ utils/drive.ts
@@ -0,0 +1,28 @@
+// Call an external API endpoint to get posts
+// const drive = google.drive({
+// 	version: 'v3',
+// 	headers: {
+// 		Authorization:
+// 			'Bearer ya29.a0ARrdaM-KnDRTh6dtmXXVfdgDfJlF37KfcWaPdAnQqs8XGaHn6Rdvn7qJexUKLBgswqhkEs6z1GushLpEC7l50XlOKa9NkmzY3_YL5jQVFqpoGGmOkYON2ZqHhbajPOr_fBRGDUPKcZWr09121F_QG4bpbG8r',
+// 	},
+// });
+
+// var fileMetadata = {
+// 	name: 'BlogData',
+// 	mimeType: 'application/vnd.google-apps.folder',
+// };
+// drive.files.create(
+// 	{
+// 		resource: fileMetadata,
+// 		fields: 'id',
+// 	},
+// 	function (err, file) {
+// 		if (err) {
+// 			// Handle error
+// 			console.error(err);
+// 		} else {
+// 			console.log({ file });
+// 			console.log('Folder Id: ', file.id);
+// 		}
+// 	}
+// );
