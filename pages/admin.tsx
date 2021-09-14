156a6e25975046824e47886efa355ead5ceab10c
zeyadetman <zeyadetman@gmail.com>
2021-09-14 05:07:53 +0200

    update

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -11,37 +11,7 @@ interface Props {}
 export async function getServerSideProps(ctx: NextPageContext) {
 	const session = await getSession(ctx);
 	console.log({ session });
-	// Call an external API endpoint to get posts
-	// const drive = google.drive({
-	// 	version: 'v3',
-	// 	headers: {
-	// 		Authorization:
-	// 			'Bearer ya29.a0ARrdaM-KnDRTh6dtmXXVfdgDfJlF37KfcWaPdAnQqs8XGaHn6Rdvn7qJexUKLBgswqhkEs6z1GushLpEC7l50XlOKa9NkmzY3_YL5jQVFqpoGGmOkYON2ZqHhbajPOr_fBRGDUPKcZWr09121F_QG4bpbG8r',
-	// 	},
-	// });
 
-	// var fileMetadata = {
-	// 	name: 'BlogData',
-	// 	mimeType: 'application/vnd.google-apps.folder',
-	// };
-	// drive.files.create(
-	// 	{
-	// 		resource: fileMetadata,
-	// 		fields: 'id',
-	// 	},
-	// 	function (err, file) {
-	// 		if (err) {
-	// 			// Handle error
-	// 			console.error(err);
-	// 		} else {
-	// 			console.log({ file });
-	// 			console.log('Folder Id: ', file.id);
-	// 		}
-	// 	}
-	// );
-
-	// By returning { props: { posts } }, the Blog component
-	// will receive `posts` as a prop at build time
 	return {
 		props: {
 			google: 'JSON.stringify(drive)',
