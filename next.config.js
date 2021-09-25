6110ac1c8631a6e4a485dd70256f8be6f0a77beb
zeyadetman <zeyadetman@gmail.com>
2021-09-25 09:18:36 +0200

    handle gallery, and blog

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 const withPlugins = require('next-compose-plugins');
+const imageminMozjpeg = require('imagemin-mozjpeg');
+const optimizedImages = require('next-optimized-images');
 
 const nextConfig = {
 	webpack: (config, options) => {
@@ -21,4 +23,40 @@ const nextConfig = {
 	},
 };
 
-module.exports = withPlugins([], nextConfig);
+module.exports = withPlugins(
+	[
+		[
+			optimizedImages,
+			{
+				// these are the default values so you don't have to provide them if they are good enough for your use-case.
+				// but you can overwrite them here with any valid value you want.
+				inlineImageLimit: 8192,
+				imagesFolder: 'images',
+				imagesName: '[name]-[hash].[ext]',
+				handleImages: ['jpeg', 'png', 'svg', 'webp', 'gif'],
+				removeOriginalExtension: false,
+				optimizeImages: true,
+				optimizeImagesInDev: true,
+				mozjpeg: {
+					quality: 80,
+				},
+				optipng: {
+					optimizationLevel: 3,
+				},
+				pngquant: false,
+				gifsicle: {
+					interlaced: true,
+					optimizationLevel: 3,
+				},
+				svgo: {
+					// enable/disable svgo plugins here
+				},
+				webp: {
+					preset: 'default',
+					quality: 75,
+				},
+			},
+		],
+	],
+	nextConfig
+);
