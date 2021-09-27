94b9a5b7e2c2732a67d65f2654d463c40dcaaf0d
zeyadetman <zeyadetman@gmail.com>
2021-09-27 10:40:05 +0200

    add analytics

--- libs/gtag.ts
+++ libs/gtag.ts
@@ -3,6 +3,7 @@ export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS;
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: URL): void => {
 	if (typeof window !== undefined && window.gtag) {
+		//@ts-ignore
 		window.gtag('config', GA_TRACKING_ID, {
 			page_path: url,
 		});
