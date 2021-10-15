06324836c205136df4ba47e688e3b3cb692a4b3d
zeyadetman <zeyadetman@gmail.com>
2021-10-15 06:59:49 +0200

    update location and weather

--- components/ColorModeIcon/index.tsx
+++ components/ColorModeIcon/index.tsx
@@ -1,11 +1,11 @@
 import { useColorMode } from '@chakra-ui/color-mode';
 import Icon from '@chakra-ui/icon';
-import { Tooltip } from '@chakra-ui/react';
 import React, { useEffect, useState } from 'react';
 import { IoMoonOutline, IoPartlySunnyOutline } from 'react-icons/io5';
 import { RiMoonCloudyLine } from 'react-icons/ri';
 import { FaCloudMoonRain } from 'react-icons/fa';
 import { WiDaySunny, WiDayRainMix } from 'react-icons/wi';
+import { getCookie, setCookie } from '../../utils/cookies';
 interface Props {}
 
 const weatherIcons = {
@@ -22,32 +22,63 @@ const weatherIcons = {
 		night: <RiMoonCloudyLine />,
 	},
 };
+
+const getLocation = async () => {
+	try {
+		const data = await (await fetch('http://ip-api.com/json')).json();
+		setCookie('latitude', data.lat);
+		setCookie('longitude', data.lon);
+		return data;
+	} catch (e) {
+		throw new Error('fail');
+	}
+};
+
 function ColorModeIcon(props: Props) {
 	const {} = props;
 	const { colorMode, toggleColorMode } = useColorMode();
 	const [weatherIcon, setWeatherIcon] = useState(weatherIcons.default);
 
-	useEffect((): void => {
-		try {
-			navigator?.geolocation?.getCurrentPosition(
-				async ({ coords: { longitude, latitude } }: GeolocationPosition) => {
-					const body = await (
-						await fetch(
-							`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=87eb04e63a1d2d86f382d92a06242e9c`
-						)
-					).json();
-
-					if (body.rain) {
-						setWeatherIcon(weatherIcons.rain);
-					} else if (body?.clouds?.all > 0) {
-						setWeatherIcon(weatherIcons.cloud);
-					} else {
-						setWeatherIcon(weatherIcons.default);
-					}
-				}
-			);
-		} catch (e) {
+	const applyWeatherIcon = (body: any) => {
+		if (body.rain) {
+			setCookie('weather', 'rain');
+			setWeatherIcon(weatherIcons.rain);
+		} else if (body?.clouds?.all > 0) {
+			setCookie('weather', 'cloud');
+			setWeatherIcon(weatherIcons.cloud);
+		} else {
+			setCookie('weather', 'default');
 			setWeatherIcon(weatherIcons.default);
+		}
+	};
+
+	useEffect((): void => {
+		const lat = getCookie('latitude');
+		const lon = getCookie('longitude');
+
+		if (!(lat && lon)) {
+			getLocation()
+				.then(async ({ lat, lon }) => {
+					try {
+						const body = await (
+							await fetch(
+								`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=87eb04e63a1d2d86f382d92a06242e9c`
+							)
+						).json();
+
+						applyWeatherIcon(body);
+					} catch (error) {
+						throw new Error('Error');
+					}
+				})
+				.catch(() => {
+					setCookie('weather', 'default');
+					setWeatherIcon(weatherIcons.default);
+				});
+		} else {
+			const weather: 'default' | 'rain' | 'cloud' =
+				getCookie('weather') || 'default';
+			setWeatherIcon(weatherIcons[weather]);
 		}
 	}, []);
 
--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,5 +1,4 @@
 import { ReactNode } from 'react';
-import LinkRoute from 'next/link';
 import {
 	Box,
 	Flex,
