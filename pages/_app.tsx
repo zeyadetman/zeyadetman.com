5ba69729f5697369a533ffce1d13b2489cf8d683
zeyadetman <zeyadetman@gmail.com>
2022-06-08 20:13:52 +0200

    Using Absolute paths

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
-import { theme } from "../theme";
+import { theme } from "theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
