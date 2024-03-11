345d290e19c740cdfec4a7847ef2bbc6e5831d77
zeyadetman <zeyadetman@gmail.com>
2024-03-11 15:38:36 +0200

    show ar badge

--- src/app/(subpages)/posts/page.tsx
+++ src/app/(subpages)/posts/page.tsx
@@ -1,7 +1,14 @@
 import { SearchPostsInput } from "@/app/components/post/search";
 import { listPosts } from "@/lib/listPosts";
 import { formatDate } from "@/utils/date";
+import { Tajawal } from "next/font/google";
 import Link from "next/link";
+
+const tajawal = Tajawal({
+  subsets: ["arabic"],
+  weight: ["400"],
+  variable: "--font-ibm-plex-sans-arabic",
+});
 
 export default function Blog({
   searchParams,
@@ -18,18 +25,29 @@ export default function Blog({
       <SearchPostsInput />
 
       <ul className="menu p-0 mt-0">
-        {listPosts({ query: searchParams.q }).map((post) => (
-          <article key={post._id} className="card post-viewer">
-            <Link href={post.slug} className="no-underline">
-              <h3 className="text-primary-content text-[1rem] decoration-none my-0">
-                {post.title}
-              </h3>
-              {post.date && (
-                <p className="text-xs my-0">{formatDate(post.date)}</p>
-              )}
-            </Link>
-          </article>
-        ))}
+        {listPosts({ query: searchParams.q }).map((post) => {
+          const baseClassName =
+            "text-primary-content text-[1rem] decoration-none my-0";
+          const className =
+            baseClassName +
+            (post?.lang === "ar" ? ` font-extrabold ${tajawal.className}` : ``);
+
+          return (
+            <article key={post._id} className="card post-viewer">
+              <Link href={post.slug} className="no-underline">
+                <h3 className={className}>{post.title}</h3>
+                <div className="flex justify-between">
+                  {post.date && (
+                    <p className="text-xs my-0">{formatDate(post.date)}</p>
+                  )}
+                  {post.lang === "ar" && (
+                    <span className="badge">{post.lang}</span>
+                  )}
+                </div>
+              </Link>
+            </article>
+          );
+        })}
       </ul>
     </div>
   );
