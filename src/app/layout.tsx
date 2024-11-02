88f4585b9a0bafda9d0cfb49fba164d3feacb8a2
zeyadetman <zeyadetman@gmail.com>
2024-11-02 06:54:06 +0200

    add progress bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { Header } from "@/app/_components/layout/header";
 import { Footer } from "@/app/_components/layout/footer";
 import { Analytics as VercelAnalytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import ProgressBarProvider from "@/app/_components/providers/progress-bar-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -33,14 +34,16 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <ThemeProvider enableColorScheme enableSystem defaultTheme="system">
-          <div className="container">
-            <Header />
-            <main className="flex min-h-[calc(100vh-20rem)] flex-col justify-between px-6 ">
-              {children}
-            </main>
-            <Footer />
-          </div>
-          <Vercel />
+          <ProgressBarProvider>
+            <div className="container">
+              <Header />
+              <main className="flex min-h-[calc(100vh-20rem)] flex-col justify-between px-6 ">
+                {children}
+              </main>
+              <Footer />
+            </div>
+            <Vercel />
+          </ProgressBarProvider>
         </ThemeProvider>
       </body>
     </html>
